298d7b593950ab54c46b4bd20c2a21bb0bce6f52
lemime <kam.kolenda@gmail.com>
2019-01-18 23:02:21 +0100

    Allow PATCH

--- src/main/java/com/avocudo/avocudoapi/rest/RestConfig.java
+++ src/main/java/com/avocudo/avocudoapi/rest/RestConfig.java
@@ -35,6 +35,7 @@ public class RestConfig {
         config.addAllowedMethod("POST");
         config.addAllowedMethod("PUT");
         config.addAllowedMethod("DELETE");
+        config.addAllowedMethod("PATCH");
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
     }
